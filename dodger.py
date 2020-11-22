5591bceef76406fc7098fc059da37d4a67b15173
Mathias <mathias.messerli@unine.ch>
2020-11-22 16:12:34 +0100

    j'ai changé la taille des projectiles

--- dodger.py
+++ dodger.py
@@ -27,6 +27,7 @@ class Projectile(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load('bullet.png')
+        self.image = pygame.transform.scale(self.image, (20, 20))
         self.rect = self.image.get_rect()
         self.rect.bottom = y
         self.rect.centerx = x
